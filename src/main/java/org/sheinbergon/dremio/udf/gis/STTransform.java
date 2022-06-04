d0c8197b3c7b9e631843999b44c370a74358e7c0
Idan Sheinberg <ishinberg0@gmail.com>
2022-06-04 04:20:04 +0300

    Work on area/length/transform

---
+++ src/main/java/org/sheinbergon/dremio/udf/gis/STTransform.java
@@ -0,0 +1,66 @@
+/**
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ * <p>
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * <p>
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.sheinbergon.dremio.udf.gis;
+
+import com.dremio.exec.expr.SimpleFunction;
+import com.dremio.exec.expr.annotations.FunctionTemplate;
+import com.dremio.exec.expr.annotations.Output;
+import com.dremio.exec.expr.annotations.Param;
+import org.sheinbergon.dremio.udf.gis.util.GeometryTransformation;
+
+import javax.inject.Inject;
+
+@FunctionTemplate(
+    name = "ST_Transform",
+    scope = FunctionTemplate.FunctionScope.SIMPLE,
+    nulls = FunctionTemplate.NullHandling.NULL_IF_NULL)
+public class STTransform implements SimpleFunction {
+
+  private static final String CRS_TEMPLATE = "EPSG: %d";
+
+  @Param
+  org.apache.arrow.vector.holders.NullableVarBinaryHolder binaryInput;
+
+  @Param
+  org.apache.arrow.vector.holders.NullableIntHolder targetSridInput;
+
+  @Output
+  org.apache.arrow.vector.holders.NullableVarBinaryHolder binaryOutput;
+
+  @Inject
+  org.apache.arrow.memory.ArrowBuf buffer;
+
+  public void setup() {
+  }
+
+  public void eval() {
+    org.locationtech.jts.geom.Geometry geom = org.sheinbergon.dremio.udf.gis.util.FunctionHelpersXL.toGeometry(binaryInput);
+    org.locationtech.proj4j.CoordinateTransform transform = transformation(geom);
+    org.locationtech.jts.geom.Geometry result = GeometryTransformation.transform(geom, transform);
+    byte[] bytes = org.sheinbergon.dremio.udf.gis.util.FunctionHelpersXL.toBinary(result);
+    buffer = buffer.reallocIfNeeded(bytes.length);
+    org.sheinbergon.dremio.udf.gis.util.FunctionHelpersXL.populate(bytes, buffer, binaryOutput);
+  }
+
+  private org.locationtech.proj4j.CoordinateTransform transformation(final org.locationtech.jts.geom.Geometry geom) {
+    org.locationtech.proj4j.CRSFactory crsFactory = new org.locationtech.proj4j.CRSFactory();
+    org.locationtech.proj4j.CoordinateReferenceSystem sourceCrs = crsFactory.createFromName(String.format(CRS_TEMPLATE, geom.getSRID()));
+    org.locationtech.proj4j.CoordinateReferenceSystem targetCrs = crsFactory.createFromName(String.format(CRS_TEMPLATE, targetSridInput.value));
+    return new org.locationtech.proj4j.BasicCoordinateTransform(sourceCrs, targetCrs);
+  }
+}
