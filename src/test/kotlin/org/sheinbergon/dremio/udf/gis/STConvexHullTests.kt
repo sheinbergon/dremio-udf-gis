fa9562d8304feeb3b66354f597499fcb223e359a
Idan Sheinberg <ishinberg0@gmail.com>
2023-01-06 02:19:24 +0200

    Concave Hull tests

--- src/test/kotlin/org/sheinbergon/dremio/udf/gis/STConvexHullTests.kt
+++ src/test/kotlin/org/sheinbergon/dremio/udf/gis/STConvexHullTests.kt
@@ -8,13 +8,13 @@ internal class STConvexHullTests : GeometryProcessingFunSpec<STConvexHull>() {
 
   init {
     testGeometryProcessing(
-      "Calling STConvexHull on a geometry collection",
+      "Calling ST_ConvexHull on a geometry collection",
       "GEOMETRYCOLLECTION(MULTILINESTRING((100 190,10 8),(150 10, 20 30)),MULTIPOINT(50 5, 150 30, 50 10, 10 10))",
       "POLYGON((50 5,10 8,10 10,100 190,150 30,150 10,50 5))"
     )
 
     testNullGeometryProcessing(
-      "Calling STConvexHull on a NULL input"
+      "Calling ST_ConvexHull on a NULL input"
     )
   }
 
